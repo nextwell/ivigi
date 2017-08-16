0a1b0e91b36442cfcb589ff57a3b6aefe64391c3
ramnrem <30977844+ramnrem@users.noreply.github.com>
2017-08-16 21:23:07 +0300

    минус свг на телефонах

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
 	console.log("script.js loaded");
+
+	if ($(window).width() <= '576'){
+		$("#logo").attr("src","images/logo.png");
+	}
+
 	$(function(){
 	        var hh = $('.header_n').height();
 	        var fh = $('.footer_n').height(); 
@@ -15,6 +20,12 @@ $(document).ready(function(){
 	        var сh = wh - hh - fh - 32; 
 	        $('.middle').css('min-height', сh); 
 	    });
+
+	  	if ($(window).width() <= '576'){
+			$("#logo").attr("src","images/logo.png");
+		} else {
+			$("#logo").attr("src","images/logo.svg");
+		}
 	});
 	 
 	
