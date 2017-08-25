429f15da434f0595c8cf4dcccba02e02d0f3a2e1
ramnrem <30977844+ramnrem@users.noreply.github.com>
2017-08-25 21:05:16 +0300

    правки отступов, липкий футер

--- js/script.js
+++ js/script.js
@@ -1,22 +1,5 @@
 $(document).ready(function(){
 	console.log("script.js loaded");
 
-	$(function(){
-	        var hh = $('.header_n').height();
-	        var fh = $('.footer_n').height(); 
-	        var wh = $(window).height(); 
-	        var сh = wh - hh - fh - 32; 
-	        $('.middle').css('min-height', сh); 
-	});
-	$(window).resize(function(){
-	  $(function(){
-	        var hh = $('.header_n').height();
-	        var fh = $('.footer_n').height(); 
-	        var wh = $(window).height(); 
-	        var сh = wh - hh - fh - 32; 
-	        $('.middle').css('min-height', сh); 
-	    });
-
-
-	});
+	
 })
