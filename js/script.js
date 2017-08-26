6c00bbec4c132ad63d0a2341049006775307048d
ramnrem <30977844+ramnrem@users.noreply.github.com>
2017-08-26 03:21:27 +0300

    страница добавления объяв-я без js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
 	console.log("script.js loaded");
 
-	
+	/* var rows = 9;
+	$('.ta').keyup(function(){
+		if ($('.ta').prop('scrollHeight') > $('.ta').prop('clientHeight')){
+			rows++;
+			 $(this).attr('rows', rows);
+		}
+	}); */
 })
+	
